c9f34fbd3397ed24229e4ecefd62bbf67aff35f7
jujinyoung <jujinyoung1838@naver.com>
2024-01-30 09:33:56 +0900

    Modal창 예시

---
+++ board/src/components/AppModal.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  show: Boolean,
+  title: String
+});
+defineEmits(['close']);
+</script>
+
+<template>
+  <!-- Modal -->
+  <div v-if="show">
+    <div class="modal-backdrop fade show"></div>
+    <div class="modal fade show d-block"
+         tabindex="-1"
+         aria-labelledby="exampleModalLabel"
+         aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <slot name="header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1>
+              <button type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                      @click="$emit('close')"
+              ></button>
+            </slot>
+          </div>
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+          <div class="modal-footer">
+            <slot name="actions"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- board/src/views/HomeView.vue
+++ board/src/views/HomeView.vue
@@ -1,16 +1,43 @@
 <template>
   <h1>Home Page</h1>
   <button @click="goAboutPage">About 페이지로 이동</button>
+
+  <button type="button" @click="openModal">모달 열기</button>
+  <AppModal :show="show" title="게시글" @close="closeModal">
+    <template #default>
+      <p>제목: </p>
+      <p>내용: </p>
+      <p>등록일: </p>
+    </template>
+    <template #actions>
+      <button type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+              @click="closeModal"
+              >닫기</button>
+    </template>
+  </AppModal>
 </template>
 
 <script setup>
 import { useRoute, useRouter } from "vue-router";
+import AppModal from "@/components/AppModal.vue";
+import {ref} from "vue";
 
 const router = useRouter();
 const route = useRoute();
 console.log('route.name: ', route.name);
 console.log('route.path: ', route.path);
 const goAboutPage = () => router.push('about');
+
+//modalTest
+const show = ref(false);
+const openModal = () => {
+  show.value = true;
+}
+const closeModal = () => {
+  show.value = false;
+}
 </script>
 
 <style lang="scss" scoped>
